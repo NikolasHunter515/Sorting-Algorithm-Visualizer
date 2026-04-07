20e08fdc4ee7f99fa92fd828feb7a39a68ffa3da
wagmimax <wagmimax@gmail.com>
2026-04-07 14:48:33 -0400

    Array generator route

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -8,4 +8,10 @@ def create_app():
     from .routes.users import users_bp
     app.register_blueprint(users_bp, url_prefix="/api/users")
 
+    from .routes.algorithmAPI import bubble_bp
+    app.register_blueprint(bubble_bp, url_prefix="/api/bubble")
+
+    from .routes.algorithmAPI import array_bp
+    app.register_blueprint(array_bp, url_prefix="/api/array")
+
     return app
