4e65a2142f7b54b5945206f471b417bfe3d68185
Nikolas Hunter <radnik2004@gmail.com>
2026-04-10 14:31:06 -0400

    Crude homepage structure added, missing animation

--- client/src/app/authPage/page.js
+++ client/src/app/authPage/page.js
@@ -1,4 +1,4 @@
-import Navbar from "../../../components/navbar/Navbar";
+import Navbar from "../../../components/server/client/navbar/Navbar";
 import Auth from "../../../components/Login/Auth";
 import "./auth.css";
 
