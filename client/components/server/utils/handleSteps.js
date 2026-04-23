a1ee9ece24015cd616bc9df7195525eb45829740
Nikolas Hunter <radnik2004@gmail.com>
2026-04-22 23:06:27 -0400

    Unhighlighting almost all bars

--- client/components/server/utils/handleSteps.js
+++ client/components/server/utils/handleSteps.js
@@ -1,4 +1,8 @@
 
+var defaultFill = "#2117ec";
+var highlightFill = "#1F77b4";
+let queue = [];
+
 function swap(data, a, b){
     const temp = data[a];
     console.log(`swaping ${a} with ${b}`);
@@ -7,8 +11,35 @@ function swap(data, a, b){
     return data;
 }
 
-function highlight(data, index){
-    data[index].fill = "#1F77b4";
+function unhightlight(data){//not working as the fetch is not working as expected.
+//maybe this unhightlight should be called elsewhere, before swaping and replacing.
+    //queue = localStorage.getItem("highlightQueue");
+
+    console.log(`Current Queue: ${queue}`);
+    while(queue.length > 0){
+        var index = queue.shift();
+        data[index].fill = defaultFill;
+        console.log(`unhighlighting: ${data[index]} , ${index}`);
+
+    }
+}
+
+function highlight(data, indexes){
+    //could look at previous indexes and change back.
+
+
+    /*if(index > 0 && data[index].fill == "#1F77b4"){
+        data[index].fill = "#2117ec";
+    }*/
+
+    for(var i = 0; i < indexes.length; i++){
+        data[indexes[i]].fill = highlightFill;
+        queue.push(indexes[i]);
+    }
+    console.log(`updated queue: ${queue}`);
+
+    localStorage.setItem("highlightQueue", queue);// this is working just fine
+
     return data;
 }
 
@@ -19,13 +50,19 @@ function replace(data, index, value){
 
 export default function handleSteps(step, data){
     let tempDta = data.map(item => ({ ...item }));
+    
+    unhightlight(tempDta);
+
+    //console.log(queue[0]);
+    //queue.push(12);
+    //queue.push(15);
     //tempDta[1].uv = 90;
     //console.log(tempDta[1].uv);
     //console.log(step);
 
     switch(step.type){
         case "highlight":
-            tempDta = highlight(tempDta, step.indices[0]);//issue here
+            tempDta = highlight(tempDta, step.indices);//issue here
             return tempDta;
         case "swap":
             tempDta = swap(tempDta, step.indices[0], step.indices[1]);
@@ -34,6 +71,7 @@ export default function handleSteps(step, data){
             tempDta = replace(tempDta, step.index, step.value);
             return tempDta;
         case "stop":
+            unhightlight(tempDta);
             return null;
 
         default:
