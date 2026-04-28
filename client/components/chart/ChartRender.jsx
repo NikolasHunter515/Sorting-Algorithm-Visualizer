02318d3ca9c7f8df75735f58bcee207778009d56
Nikolas Hunter <radnik2004@gmail.com>
2026-04-28 15:10:46 -0400

    Background color and bar shadow added

--- client/components/chart/ChartRender.jsx
+++ client/components/chart/ChartRender.jsx
@@ -8,13 +8,28 @@ export default function ChartRender({data}){
     const [arr, setArr] = useState([1,4,5,6]);
 
     return (
-        <BarChart
-        style={{ width: '100%', maxWidth: '300px', maxHeight: '100px', aspectRatio: 1.618 }}
-        responsive
-        data={data}
-        >
-        <Bar dataKey="uv" fill="#2117ec" />
-        <RechartsDevtools />
-        </BarChart>
+        <div className="chartRend">
+                <BarChart
+            style={{ width: '100%', maxWidth: '2000px', maxHeight: '2000px', aspectRatio: 1.618 }}
+            responsive
+            data={data}
+            >
+
+            <defs>
+            <filter id="shadow" x="-100%" y="0%" width="200%" height="200%">
+            <feDropShadow
+                dx="-3"
+                dy="0"
+                stdDeviation="4"
+                floodColor="#000"
+                floodOpacity="1.25"
+            />
+            </filter>
+        </defs>
+
+            <Bar dataKey="uv" fill="#2117ec" isAnimationActive={false} filter="url(#shadow)"/>
+            <RechartsDevtools />
+            </BarChart>
+        </div>
     );
 }
