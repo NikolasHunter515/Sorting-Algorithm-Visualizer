b3b98045b9da3d3f9a11da5c7a55490176a14792
Nikolas Hunter <radnik2004@gmail.com>
2026-05-05 09:29:58 -0400

    Bounds added to input size UI

--- client/components/server/client/inputSize/SetInputSize.jsx
+++ client/components/server/client/inputSize/SetInputSize.jsx
@@ -4,13 +4,24 @@ import { useState } from 'react';
 export default function SetInputSize({ inputSize, setInputSize }) {
     const [inVal, setInVal] = useState(`N: ${inputSize}`)
 
+    //works but is kinda of buggy, new bug for irrisa
+    function validInput(size){
+        if(size >= 10 && size <= 200){
+            setInputSize(size);
+            console.log("Valid input");
+        }
+        else{
+            console.log("Invalid input");
+        }
+    }
+
     return (
         <div>
             <input
                 type="number"
                 placeholder="N: "
                 value={inputSize || ''}
-                onChange={(e) => setInputSize(e.target.value)}
+                onChange={(e) => validInput(e.target.value)}
                 id='inputSize'
             />
         </div>
