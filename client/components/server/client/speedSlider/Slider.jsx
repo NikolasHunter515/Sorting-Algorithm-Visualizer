50c9f197e2da5cc0fe7243070cf79e321ceafa9b
Nikolas Hunter <radnik2004@gmail.com>
2026-05-16 22:17:40 -0400

    Faster sorting speed added

--- client/components/server/client/speedSlider/Slider.jsx
+++ client/components/server/client/speedSlider/Slider.jsx
@@ -5,10 +5,28 @@ import Tips from "../tipPopup/Tips";
 
 export default function Slide({speed, setSpeed}){
     
+    function invertSpeed(s){
+        return (1001 - s);
+    }
+
     console.log(speed);
     return(
         <div className="slidecontainer">
-            <input type="range" min="50" max="1000" value={speed} className="slider" onChange={(e) => setSpeed(e.target.value)}/>
+            <input type="range" min="1" max="1000" value={speed} className="slider" onChange={(e) => setSpeed(e.target.value)}/>
         </div>
     );
 }
+
+//kind of working with exception being the slider.
+/*
+function invertSpeed(s){
+        return (1001 - s);
+    }
+
+    console.log(speed);
+    return(
+        <div className="slidecontainer">
+            <input type="range" min="1" max="1000" value={-speed} className="slider" onChange={(e) => setSpeed(invertSpeed(e.target.value))}/>
+        </div>
+    );
+*/
